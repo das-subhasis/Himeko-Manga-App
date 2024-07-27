8f0c84b9ba3a8bac81d18c304a40c4766223a9eb
Subhasis Das <das.shubasis2003@gmail.com>
2024-07-27 23:30:29 +0530

    ADD chapter route

--- BackEnd/routes/mangaRoutes.js
+++ BackEnd/routes/mangaRoutes.js
@@ -40,7 +40,7 @@ router.get('/manga-cover/:mangaId/:coverArtUrl', async (req, res) => {
     }
 });
 
-router.get('chapter/:mangaId/feed', asyncHandler(async (req, res) => {
+router.get('/chapter/:mangaId/feed', asyncHandler(async (req, res) => {
     const {mangaId} = req.params;
     try {
         const data = await getMangaChapter(mangaId);
